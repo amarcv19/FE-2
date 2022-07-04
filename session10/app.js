ee0ba1a71ac4167346ace76e9cdae81fe8aef6ad
Amar Chandra <amarcv19@gmail.com>
2022-07-04 15:04:56 +0530

    Session 10 activities completed

---
+++ session10/app.js
@@ -0,0 +1,15 @@
+const obj = {
+  name:'amar chandra verma',
+  age:24
+};
+
+console.log(obj);
+
+console.log(JSON.stringify(obj));
+
+console.log(JSON.parse(JSON.stringify(obj)));
+
+
+const arr =[12,34,5];
+
+console.log(JSON.stringify(arr));
